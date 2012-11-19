659b4947845b64bc06522e13831273e12a5e2e05
Alexander Smirnov <zoocide@gmail.com>
2012-11-19 22:45:32 +0400

    Exceptions: add manipulators
    + exception2string
    + string2exception

--- modules/Exceptions.pm
+++ modules/Exceptions.pm
@@ -2,7 +2,7 @@ package Exceptions;
 use base qw(Exporter);
 use Exceptions::Exception;
 our @EXPORT;
-@EXPORT = qw(try throw catch);
+@EXPORT = qw(try throw catch exception2string string2exception);
 
 our $VERSION = '0.2.0';
 
@@ -46,6 +46,10 @@ sub try (&;$)
     if ($arr){
       while (@$arr){
         my ($t, $s) = @{ shift @$arr };
+        if (!defined $t){
+          &$s($@);
+          next;
+        }
         return &$s($@) if (!$t || $@->isa($t));
       }
     }
@@ -60,6 +64,34 @@ sub catch (&;$;$)
   my $ret  = ref $_[1] ? $_[1] : (ref $_[2] ? $_[2] : []);
 
   unshift @$ret, [$type, $_[0]];
+  $ret
+}
+
+sub exception2string (;$)
+{
+  my $ret = ref $_[0] ? $_[0] : [];
+
+  my $s = sub {
+    if ($_[0] && $@->isa('Exceptions::Exception')){
+      chomp (my $msg = $_[0]->msg);
+      $_[0] = $msg."\n";
+    }
+  };
+  unshift @$ret, [undef, $s];
+  $ret
+}
+
+sub string2exception (;$)
+{
+  my $ret = ref $_[0] ? $_[0] : [];
+
+  my $s = sub {
+    if (!ref $_[0]){
+      chomp($_[0]);
+      $_[0] = Exceptions::Exception->new($_[0]);
+    }
+  };
+  unshift @$ret, [undef, $s];
   $ret
 }
 
