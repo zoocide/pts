608f0d8dbb8594ba5b239737a3051a5c3e3e202c
Alexander Smirnov <zoocide@gmail.com>
2012-11-20 23:46:05 +0400

    add Plugins::Base

---
+++ Plugins/Base.pm
@@ -0,0 +1,21 @@
+package Plugins::Base;
+use Task;
+
+=head1 SYNOPSIS
+
+  my $result = Plugins::PluginName->process($task);
+  print 'task ', ($result ? 'complete' : 'failed'), "\n";
+
+=head1 DESCRIPTION
+
+This is the base of all plugins.
+
+=cut
+
+sub process
+{
+  die $_[0].' is not implemented. Task \''.$_[1]->name."' can not be processed\n";
+}
+
+1;
+
