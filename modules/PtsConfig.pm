d18f612d8bd2048b259db6375bef248e418ed1f3
Alexander Smirnov <zoocide@gmail.com>
2016-08-01 18:11:30 +0300

    PtsConfig: trivial fix

--- modules/PtsConfig.pm
+++ modules/PtsConfig.pm
@@ -3,9 +3,12 @@ use strict;
 use FindBin;
 use File::Spec::Functions qw(catfile);
 
-my %c;
+my %c = (
+  # tasks_dir => 'default_tasks_directory',
+  # plugins_parent_dir => 'plugins_parent_direcotry', #It contains Plugins/all_plugins
+);
 
-sub tasks_dir {$c{tasks_dir} |= catfile($FindBin::Bin, '..', 'tasks')}
-sub plugins_parent_dir {$c{plugins_parent_dir}|=catfile($FindBin::Bin, '..')}
+sub tasks_dir {$c{tasks_dir} ||= catfile($FindBin::Bin, '..', 'tasks')}
+sub plugins_parent_dir {$c{plugins_parent_dir} ||= catfile($FindBin::Bin, '..')}
 
 1
