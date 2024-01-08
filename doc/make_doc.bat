3b4fba7113aa0aebdc24da696e7b4f2cc151230d
Alexander Smirnov <zoocide@gmail.com>
2024-01-08 14:13:26 +0300

    [doc] Change html directories structure to fix links generated in Windows.

--- doc/make_doc.bat
+++ doc/make_doc.bat
@@ -1,10 +1,15 @@
 @echo off
 set css_url=https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css
-set opts=--htmldir=html --podroot=../modules --podpath=. --css=%css_url%
+set opts=--htmldir=html --podroot=.. --podpath=modules --css=%css_url%
 set mod_dir=../modules/
-set html_dir=html/
+set html_dir=html/modules/
 set css=https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css
-call pod2html %opts% %mod_dir%pts.pod --outfile=%html_dir%index.html
+
+call perl -MExtUtils::Command -e mkpath %html_dir%external
+call perl -MExtUtils::Command -e mkpath %html_dir%Task
+call perl -MExtUtils::Command -e mkpath %html_dir%Plugins
+
+call pod2html %opts% %mod_dir%pts.pod --outfile=html/index.html
 call pod2html %opts% %mod_dir%TaskDB.pm --outfile=%html_dir%TaskDB.html
 call pod2html %opts% %mod_dir%Task.pm --outfile=%html_dir%Task.html
 call pod2html %opts% %mod_dir%Task/ID.pm --outfile=%html_dir%Task/ID.html
