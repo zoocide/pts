13a7487533122abeff1bc7b77c81ba43028db9ab
Alexander Smirnov <zoocide@gmail.com>
2024-01-09 15:55:07 +0300

    [Configuration] Add 'config:unset=gr::var' to remove a variable.

--- Plugins/Configuration.pm
+++ Plugins/Configuration.pm
@@ -23,7 +23,9 @@ sub help_message
   return <<EOT;
 The ${ci}$name$ce task writes its argument variables into the configuration file.
 Variables from the default ${ci}''$ce group are not saved.
+To save a variable use command ${cc}pts config:group::var=value$ce.
 To see the stored variables use command ${cc}pts config:list$ce.
+To remove a variable from the file use command ${cc}pts config:unset=group::var$ce.
 A plugin can use this task to obtain stored values this way:
 
   ${cc}my \$conf = \$db->get_task('$name')->data;$ce
@@ -41,11 +43,23 @@ sub on_prepare
   my $prepared_tasks = shift;
   my $db = shift;
 
+  my $name = $task->id->short_id;
+
   if ($task->get_var('', 'list', '')) {
     print_config($task);
     return;
   }
-  update_config($task);
+
+  my $force_update;
+  if (my @vars = $task->get_arr('', 'unset', [])) {
+    my $conf = $task->data;
+    $force_update = 1;
+    for (@vars) {
+      /^(\w+)::(\w+)$/ or die "wrong value specified: $name:unset='$_'\n";
+      $conf->unset($1, $2);
+    }
+  }
+  update_config($task, $force_update);
 }
 
 sub load_config
@@ -77,13 +91,14 @@ sub print_config
 sub update_config
 {
   my $task = shift;
+  my $force = shift;
   ## save all arguments to the config file ##
   dbg1 and dprint("update config file '", $task->data->filename, "'");
   my %args = $task->id->args;
 
   # do not save the default '' group.
   delete $args{''};
-  %args or return;
+  %args || $force or return;
 
   my $conf = $task->data;
   while (my ($gr, $vars) = each %args) {
@@ -93,9 +108,6 @@ sub update_config
   }
   dbg2 and dprint("save config file '", $conf->filename, "'");
   $conf->save;
-
-  ## update the task object data ##
-  $task->set_data($conf);
 }
 
 1
