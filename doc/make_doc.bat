0a31e7b1a36a72899f0204874030421c43273ded
Alexander Smirnov <zoocide@gmail.com>
2024-01-12 22:20:20 +0300

    [doc] Add a page for Plugins::Base::Util.

--- doc/make_doc.bat
+++ doc/make_doc.bat
@@ -8,12 +8,14 @@ set css=https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css
 call perl -MExtUtils::Command -e mkpath %html_dir%external
 call perl -MExtUtils::Command -e mkpath %html_dir%Task
 call perl -MExtUtils::Command -e mkpath %html_dir%Plugins
+call perl -MExtUtils::Command -e mkpath %html_dir%Plugins/Base
 
 call pod2html %opts% %mod_dir%pts.pod --outfile=html/index.html
 call pod2html %opts% %mod_dir%TaskDB.pm --outfile=%html_dir%TaskDB.html
 call pod2html %opts% %mod_dir%Task.pm --outfile=%html_dir%Task.html
 call pod2html %opts% %mod_dir%Task/ID.pm --outfile=%html_dir%Task/ID.html
 call pod2html %opts% %mod_dir%Plugins/Base.pm --outfile=%html_dir%Plugins/Base.html
+call pod2html %opts% %mod_dir%Plugins/Base/Util.pm --outfile=%html_dir%Plugins/Base/Util.html
 call pod2html %opts% %mod_dir%external/ConfigFile.pm --outfile=%html_dir%external/ConfigFile.html
 call pod2html %opts% %mod_dir%external/ConfigFileScheme.pm --outfile=%html_dir%external/ConfigFileScheme.html
 call pod2html %opts% %mod_dir%external/CmdArgs.pm --outfile=%html_dir%external/CmdArgs.html
